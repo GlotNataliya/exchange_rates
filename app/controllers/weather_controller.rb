58de508ed6d88d00accde6857459bf4cd86db841
Nataliya Glotova <an.chishevich@yandex.ru>
2022-08-13 23:20:09 +0300

    Refactoring weather code

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,17 +2,15 @@ class WeatherController < ApplicationController
   include HTTParty
 
   def index
-    url_minsk = Rails.application.credentials.regions.fetch(:url_minsk, '').freeze
-    url_vitebsk = Rails.application.credentials.regions.fetch(:url_vitebsk, '').freeze
-    url_brest = Rails.application.credentials.regions.fetch(:url_brest, '').freeze
-    url_mogilev = Rails.application.credentials.regions.fetch(:url_mogilev, '').freeze
-    url_gomel = Rails.application.credentials.regions.fetch(:url_gomel, '').freeze
-    url_grodno = Rails.application.credentials.regions.fetch(:url_grodno, '').freeze
-    @weather_minsk = HTTParty.get(url_minsk)
-    @weather_vitebsk = HTTParty.get(url_vitebsk)
-    @weather_brest = HTTParty.get(url_brest)
-    @weather_mogilev = HTTParty.get(url_mogilev)
-    @weather_gomel = HTTParty.get(url_gomel)
-    @weather_grodno = HTTParty.get(url_grodno)
+    api_key = Rails.application.credentials.fetch(:api_key, '').freeze
+
+    @weather_minsk = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?id=625143&lang=un&units=metric' + api_key)
+    @weather_vitebsk = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?id=620127&lang=un&units=metric' + api_key)
+    @weather_brest = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?id=629634&lang=un&units=metric' + api_key)
+    @weather_mogilev = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?id=625665&lang=un&units=metric' + api_key)
+    @weather_gomel = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?id=627907&lang=un&units=metric' + api_key)
+    @weather_grodno = HTTParty.get('http://api.openweathermap.org/data/2.5/weather?id=627904&lang=un&units=metric' + api_key)
+
+    @weathers = [ @weather_minsk, @weather_vitebsk, @weather_brest, @weather_mogilev, @weather_gomel, @weather_grodno ]
   end
 end
